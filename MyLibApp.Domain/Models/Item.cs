2aa929eb83e3c9f430436d292a9e81319c30747d
PiotrStudzinski <piotr.studzinski@gmail.com>
2021-02-12 21:18:42 +0100

    Adding Model Item

---
+++ MyLibApp.Domain/Models/Item.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyLibApp.Domain.Models
+{
+    public class Item
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public int ItemTypeId { get; set; }
+        public virtual ItemType ItemType { get; set; }
+        public int CategoryId { get; set; }
+        public virtual Category Category { get; set; }
+        public int AuthorId { get; set; }
+        public virtual Author Author { get; set; }
+    }
+}
