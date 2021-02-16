fc1abd6e3894120fa19c52bf730200d0a66cad45
PiotrStudzinski <piotr.studzinski@gmail.com>
2021-02-16 13:51:37 +0100

    Adding Context

---
+++ MyLibApp.Infrastructure/Context.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using MyLibApp.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyLibApp.Infrastructure
+{
+    public class Context : IdentityDbContext
+    {
+        public DbSet<Author> Authors { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Item> Items { get; set; }
+        public DbSet<ItemType> ItemTypes { get; set; }
+
+        public Context(DbContextOptions options) :base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+        }
+    }
+}
