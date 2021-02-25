efb9ba1121109c8677febf66bc6041a2030a4a2b
PiotrStudzinski <piotr.studzinski@gmail.com>
2021-02-25 20:23:25 +0100

    Add Interface IItemRepository

---
+++ MyLibApp.Domain/Interfaces/IItemRepository.cs
@@ -0,0 +1,28 @@
+﻿using MyLibApp.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MyLibApp.Domain.Interfaces
+{
+    public interface IItemRepository
+    {
+        int AddItem(Item item);
+
+        void DeleteItem(int itemId);
+
+        void UpdateItem(Item item);
+
+        Item GetItemById(int itemId);
+
+        IQueryable<Item> GetAllItems();
+
+        IQueryable<Item> GetItemByAuthorId(int authorId);
+
+        IQueryable<Item> GetItemByCategoryId(int categoryId);
+
+        IQueryable<Item> GetItemByItemTypeId(int itemTypeId);
+
+    }
+}
