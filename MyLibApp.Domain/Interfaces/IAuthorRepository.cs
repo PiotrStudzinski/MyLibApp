0346cc9d16ac4b01dc522f4ff6d26ac8e3f8f3e5
PiotrStudzinski <piotr.studzinski@gmail.com>
2021-02-25 20:20:26 +0100

    Add Interface IAuthorRepository

---
+++ MyLibApp.Domain/Interfaces/IAuthorRepository.cs
@@ -0,0 +1,21 @@
+﻿using MyLibApp.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MyLibApp.Domain.Interfaces
+{
+    public interface IAuthorRepository
+    {
+        int AddAuthor(Author author);
+
+        void DeleteAuthor(int authorId);
+
+        void UpdateAuthor(Author author);
+
+        Author GetAuthorById(int authorId);
+
+        IQueryable<Author> GetAllAuthors();
+    }
+}
