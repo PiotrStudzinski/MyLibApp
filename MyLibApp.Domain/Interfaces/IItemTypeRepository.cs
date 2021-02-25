8b1fce94e49a10abfa5085e347e5bfd6771b2bca
PiotrStudzinski <piotr.studzinski@gmail.com>
2021-02-25 20:24:14 +0100

    Add Interface IItemTypeRepository

---
+++ MyLibApp.Domain/Interfaces/IItemTypeRepository.cs
@@ -0,0 +1,21 @@
+﻿using MyLibApp.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MyLibApp.Domain.Interfaces
+{
+    public interface IItemTypeRepository
+    {
+        int AddItemType(ItemType itemType);
+
+        void DeleteItemType(int itemTypeId);
+
+        void UpdateItemType(ItemType itemType);
+
+        ItemType GetItemTypeById(int itemTypeId);
+
+        IQueryable<ItemType> GetAllItemTypes();
+    }
+}
