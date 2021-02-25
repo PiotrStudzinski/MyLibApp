a2929db791f490281c6eba830f9950cd0c1e6bc9
PiotrStudzinski <piotr.studzinski@gmail.com>
2021-02-25 20:39:00 +0100

    Adding Interface IAuthorService

---
+++ MyLibApp.Application/Interfaces/IAuthorService.cs
@@ -0,0 +1,14 @@
+﻿using MyLibApp.Application.ViewModels.Author;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyLibApp.Application.Interfaces
+{
+    public interface IAuthorService
+    {
+        AuthorListVm GetAllAuthorForList();
+        int AddAuthor(AuthorNewVm author);
+        AuthorDetailsVm GetAuthorDetails(int authorId);
+    }
+}
