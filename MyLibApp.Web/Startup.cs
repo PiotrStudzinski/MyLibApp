60f7aa882459e8df49d0b89f659f0c86699e6bd1
PiotrStudzinski <piotr.studzinski@gmail.com>
2021-02-25 20:50:57 +0100

    Adding and configuring FluentValidation

--- MyLibApp.Web/Startup.cs
+++ MyLibApp.Web/Startup.cs
@@ -1,3 +1,4 @@
+using FluentValidation.AspNetCore;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -37,7 +38,7 @@ namespace MyLibApp.Web
             services.AddApplication();
             services.AddInfrastructure();
 
-            services.AddControllersWithViews();
+            services.AddControllersWithViews().AddFluentValidation();
             services.AddRazorPages();
         }
 
