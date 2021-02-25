82bda412876f9c4dcaf6f2330f535cb558236115
PiotrStudzinski <piotr.studzinski@gmail.com>
2021-02-25 20:26:41 +0100

    Add Repository CategoryRepopsitory

---
+++ MyLibApp.Infrastructure/Repositories/CategoryRepository.cs
@@ -0,0 +1,56 @@
+﻿using MyLibApp.Domain.Interfaces;
+using MyLibApp.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MyLibApp.Infrastructure.Repositories
+{
+    public class CategoryRepository : ICategoryRepository
+    {
+        private readonly Context _context;
+
+        public CategoryRepository(Context context)
+        {
+            _context = context;
+        }
+        
+        public int AddCategory(Category category)
+        {
+            _context.Categories.Add(category);
+            _context.SaveChanges();
+
+            return category.Id;
+        }
+
+        public void DeleteCategory(int categoryId)
+        {
+            var category = _context.Categories.Find(categoryId);
+
+            if (category != null)
+            {
+                _context.Categories.Remove(category);
+                _context.SaveChanges();
+            }
+        }
+
+        public IQueryable<Category> GetAllCategories()
+        {
+            return _context.Categories;
+        }
+
+        public Category GetCategoryById(int categoryId)
+        {
+            var category = _context.Categories.FirstOrDefault(c => c.Id == categoryId);
+
+            return category;
+        }
+
+        public void UpdateCategory(Category category)
+        {
+            _context.Categories.Update(category);
+            _context.SaveChanges();
+        }
+    }
+}
