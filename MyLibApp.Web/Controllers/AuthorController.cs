cd5ffcdbcdceb68aabc8f81366f717282cf7e35b
PiotrStudzinski <piotr.studzinski@gmail.com>
2021-02-25 20:42:49 +0100

    Adding Controller AuthorController

---
+++ MyLibApp.Web/Controllers/AuthorController.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MyLibApp.Application.Interfaces;
+using MyLibApp.Application.ViewModels.Author;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyLibApp.Web.Controllers
+{
+    public class AuthorController : Controller
+    {
+        private readonly IAuthorService _authorService;
+
+        public AuthorController(IAuthorService authorService)
+        {
+            _authorService = authorService;
+        }
+
+        [HttpGet]
+        public IActionResult Index()
+        {
+            var model = _authorService.GetAllAuthorForList();
+
+            return View(model);
+        }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View(new AuthorNewVm());
+        }
+
+        [HttpPost]
+        public IActionResult Create(AuthorNewVm model)
+        {
+            var id = _authorService.AddAuthor(model);
+
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet]
+        public IActionResult Details(int id)
+        {
+            var model = _authorService.GetAuthorDetails(id);
+
+            return View(model);
+        }
+    }
+}
