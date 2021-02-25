b23e48020971e510fa3c3a826e4a6fc9314e115b
PiotrStudzinski <piotr.studzinski@gmail.com>
2021-02-25 20:21:43 +0100

    Add Interface ICategoryRepository

---
+++ MyLibApp.Domain/Interfaces/ICategoryRepository.cs
@@ -0,0 +1,21 @@
+﻿using MyLibApp.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MyLibApp.Domain.Interfaces
+{
+    public interface ICategoryRepository
+    {
+        int AddCategory(Category category);
+
+        void DeleteCategory(int categoryId);
+
+        void UpdateCategory(Category category);
+
+        Category GetCategoryById(int categoryId);
+
+        IQueryable<Category> GetAllCategories();
+    }
+}
