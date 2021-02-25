0c37bacf4b840b8b1eb1692107bec553b10bac02
PiotrStudzinski <piotr.studzinski@gmail.com>
2021-02-25 20:39:48 +0100

    Adding Service AuthorService

---
+++ MyLibApp.Application/Services/AuthorService.cs
@@ -0,0 +1,53 @@
+﻿using AutoMapper;
+using AutoMapper.QueryableExtensions;
+using MyLibApp.Application.Interfaces;
+using MyLibApp.Application.ViewModels.Author;
+using MyLibApp.Domain.Interfaces;
+using MyLibApp.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MyLibApp.Application.Services
+{
+    public class AuthorService : IAuthorService
+    {
+        private readonly IAuthorRepository _authorRepository;
+        private readonly IMapper _mapper;
+
+        public AuthorService(IAuthorRepository authorRepository, IMapper mapper)
+        {
+            _authorRepository = authorRepository;
+            _mapper = mapper;
+        }
+        
+        public int AddAuthor(AuthorNewVm author)
+        {
+            var newAuthor = _mapper.Map<Author>(author);
+            var id = _authorRepository.AddAuthor(newAuthor);
+
+            return id;
+        }
+
+        public AuthorListVm GetAllAuthorForList()
+        {
+            var authors = _authorRepository.GetAllAuthors()
+                .ProjectTo<AuthorForListVm>(_mapper.ConfigurationProvider).ToList();
+            var authorList = new AuthorListVm()
+            {
+                Authors = authors
+            };
+
+            return authorList;
+        }
+
+        public AuthorDetailsVm GetAuthorDetails(int authorId)
+        {
+            var author = _authorRepository.GetAuthorById(authorId);
+            var authorVm = _mapper.Map<AuthorDetailsVm>(author);
+
+            return authorVm;
+        }
+    }
+}
