e3b0646179fe62d4eb0e4a3767e9388034a2b1e7
PiotrStudzinski <piotr.studzinski@gmail.com>
2021-02-27 07:16:37 +0100

    Update AuthorNewVm

--- MyLibApp.Application/ViewModels/Author/AuthorNewVm.cs
+++ MyLibApp.Application/ViewModels/Author/AuthorNewVm.cs
@@ -1,7 +1,9 @@
 ﻿using AutoMapper;
+using FluentValidation;
 using MyLibApp.Application.Mapping;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Text;
 
 namespace MyLibApp.Application.ViewModels.Author
@@ -9,12 +11,26 @@ namespace MyLibApp.Application.ViewModels.Author
     public class AuthorNewVm : IMapFrom<MyLibApp.Domain.Models.Author>
     {
         public int Id { get; set; }
+        [DisplayName("First Name")]
         public string FirstName { get; set; }
+        [DisplayName("Last Name")]
         public string LastName { get; set; }
 
         public void Mapping(Profile profile)
         {
-            profile.CreateMap<AuthorNewVm, MyLibApp.Domain.Models.Author>();
+            profile.CreateMap<AuthorNewVm, MyLibApp.Domain.Models.Author>().ReverseMap();
+        }
+    }
+
+    public class AuthorNewValidation : AbstractValidator<AuthorNewVm>
+    {
+        public AuthorNewValidation()
+        {
+            RuleFor(x => x.Id).NotNull();
+            RuleFor(x => x.FirstName).NotNull();
+            RuleFor(x => x.FirstName).MaximumLength(80);
+            RuleFor(x => x.LastName).NotNull();
+            RuleFor(x => x.LastName).MaximumLength(100);
         }
     }
 }
