d93e18584bd785c74011f55d456c18fdb36fd26f
PiotrStudzinski <piotr.studzinski@gmail.com>
2021-03-08 06:46:11 +0100

    Adding ViewModels ItemTypeForListVm

---
+++ MyLibApp.Application/ViewModels/ItemType/ItemTypeForListVm.cs
@@ -0,0 +1,19 @@
+﻿using AutoMapper;
+using MyLibApp.Application.Mapping;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyLibApp.Application.ViewModels.ItemType
+{
+    public class ItemTypeForListVm : IMapFrom<MyLibApp.Domain.Models.ItemType>
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        public void Mapping(Profile profile)
+        {
+            profile.CreateMap<MyLibApp.Domain.Models.ItemType, ItemTypeForListVm>();
+        }
+    }
+}
